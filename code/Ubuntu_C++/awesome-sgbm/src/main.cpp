b3e805c022fd1f85592306f1e49cb923abe0bc83
wdxm <473384898@qq.com>
2023-02-19 06:00:49 +0000

    git ignore update

--- code/Ubuntu_C++/awesome-sgbm/src/main.cpp
+++ code/Ubuntu_C++/awesome-sgbm/src/main.cpp
@@ -1,69 +1,104 @@
 /* -*-c++-*- SemiGlobalMatching - Copyright (C) 2022.
-* Author	: EscapeTHU
-*/
-#include<SemiGlobalMatching.h>
-#include<opencv2/opencv.hpp>
-#include<fstream>
-#include<stdio.h>
+ * Author	: EscapeTHU
+ */
+#include <SemiGlobalMatching.h>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+#include <stdio.h>
+#include <unistd.h>
+#include <vector>
 
-int main(int argv, char** argc)
+std::vector<std::string> splitString(const std::string &str, const std::string &delimiter)
 {
-  if(argv < 3)
-   {
-     std::cout<<"please input the left and right image path..."<<std::endl;
-     return -1;
-   }
-  
-   //read image
-   std::string path_left = argc[1];
-   std::string path_right = argc[2];
-   
-   cv::Mat img_left_c = cv::imread(path_left,cv::IMREAD_COLOR);
-   cv::Mat img_left = cv::imread(path_left,cv::IMREAD_GRAYSCALE);
-   cv::Mat img_right = cv::imread(path_right,cv::IMREAD_GRAYSCALE);
-   
-   cv::imwrite("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/grey_left.png", img_left);
-   cv::imwrite("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/grey_right.png", img_right);
-   
-   cv::namedWindow("img_left_c",1);
-   cv::imshow("img_left_c",img_left_c);
+    std::vector<std::string> result;
+    size_t start = 0, end = 0;
+    while ((end = str.find(delimiter, start)) != std::string::npos)
+    {
+        result.push_back(str.substr(start, end - start));
+        start = end + delimiter.length();
+    }
+    result.push_back(str.substr(start));
+    return result;
+}
 
-   cv::waitKey(30);
+int main(int argv, char **argc)
+{
 
-   cv::namedWindow("img_left",1);
-   cv::imshow("img_left",img_left);
+    char cwd[256];
+    if (getcwd(cwd, sizeof(cwd)) == nullptr)
+    {
+        std::cerr << "Failed to get current working directory" << std::endl;
+        return 1;
+    }
+    std::string cwd_ = std::string(cwd);
+    cwd_ = (cv::String)splitString(cwd_, "awesome-sgbm").at(0);
 
-   cv::waitKey(30);
+    if (argv < 3)
+    {
+        std::cout << "please input the left and right image path..." << std::endl;
+        return -1;
+    }
 
-   cv::namedWindow("img_right",1);
-   cv::imshow("img_right",img_right);
+    // read image
+    std::string path_left = argc[1];
+    std::string path_right = argc[2];
 
-   cv::waitKey(30);
-  
-   
-   if (img_left.data == nullptr || img_right.data == nullptr) {
+    cv::Mat img_left_c = cv::imread(path_left, cv::IMREAD_COLOR);
+    cv::Mat img_left = cv::imread(path_left, cv::IMREAD_GRAYSCALE);
+    cv::Mat img_right = cv::imread(path_right, cv::IMREAD_GRAYSCALE);
+
+    // save image
+    cv::String pl = "awesome-sgbm/Data/FPGA_proc/grey_left.png";
+    cv::String pr = "awesome-sgbm/Data/FPGA_proc/grey_right.png";
+    pl = cwd_ + pl;
+    pr = cwd_ + pr;
+    
+    cv::imwrite(pl, img_left);
+    cv::imwrite(pr, img_right);
+
+    cv::namedWindow("img_left_c", 1);
+    cv::imshow("img_left_c", img_left_c);
+
+    cv::waitKey(30);
+
+    cv::namedWindow("img_left", 1);
+    cv::imshow("img_left", img_left);
+
+    cv::waitKey(30);
+
+    cv::namedWindow("img_right", 1);
+    cv::imshow("img_right", img_right);
+
+    cv::waitKey(30);
+
+    if (img_left.data == nullptr || img_right.data == nullptr)
+    {
         std::cout << "fail to read images" << std::endl;
         return -1;
-   }
-   if (img_left.rows != img_right.rows || img_left.cols != img_right.cols) {
+    }
+    if (img_left.rows != img_right.rows || img_left.cols != img_right.cols)
+    {
         std::cout << "img_left.size not equals img_right" << std::endl;
         return -1;
-   }
+    }
 
-   const sint32 width = static_cast<uint32>(img_left.cols);
-   const sint32 height = static_cast<uint32>(img_right.rows);
-   std::cout<<"width of input picture == "<<width<<std::endl;
-   std::cout<<"height of input picture == "<<height<<std::endl;
+    const sint32 width = static_cast<uint32>(img_left.cols);
+    const sint32 height = static_cast<uint32>(img_right.rows);
+    std::cout << "width of input picture == " << width << std::endl;
+    std::cout << "height of input picture == " << height << std::endl;
+    return 0;
 
-   //the graydata of the left and right image
-   auto bytes_left = new uint8[width * height];
-   auto bytes_right = new uint8[width * height];
-   for (int i = 0; i < height; i++) {
-       for (int j = 0; j < width; j++) {
+    // the graydata of the left and right image
+    auto bytes_left = new uint8[width * height];
+    auto bytes_right = new uint8[width * height];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             bytes_left[i * width + j] = img_left.at<uint8>(i, j);
             bytes_right[i * width + j] = img_right.at<uint8>(i, j);
-       }
-   }
+        }
+    }
     // SGM匹配参数设计
     SemiGlobalMatching::SGMOption sgm_option;
     // 聚合路径数
@@ -71,10 +106,10 @@ int main(int argv, char** argc)
     // 候选视差范围
     sgm_option.min_disparity = argv < 4 ? 0 : atoi(argc[3]);
     sgm_option.max_disparity = argv < 5 ? 64 : atoi(argc[4]);
-    std::cout<<"argc[3]== "<<argc[3]<<std::endl;
-    std::cout<<"argc[4]== "<<argc[4]<<std::endl;
-    std::cout<<"sgm_option.min_disparity == "<<sgm_option.min_disparity<<std::endl;
-    std::cout<<"sgm_option.max_disparity == "<<sgm_option.max_disparity<<std::endl;
+    std::cout << "argc[3]== " << argc[3] << std::endl;
+    std::cout << "argc[4]== " << argc[4] << std::endl;
+    std::cout << "sgm_option.min_disparity == " << sgm_option.min_disparity << std::endl;
+    std::cout << "sgm_option.max_disparity == " << sgm_option.max_disparity << std::endl;
     // census窗口类型
     sgm_option.census_size = SemiGlobalMatching::Census5x5;
     // 一致性检查
@@ -94,115 +129,136 @@ int main(int argv, char** argc)
 
     // 定义SGM匹配类实例
     SemiGlobalMatching sgm;
-    
+
     // 初始化
-    if (!sgm.Initialize(width, height, sgm_option)) {
+    if (!sgm.Initialize(width, height, sgm_option))
+    {
         std::cout << "SGM初始化失败！" << std::endl;
         return -2;
     }
-    
+
     // 匹配
     auto disparity = new float32[uint32(width * height)]();
     auto disparity_temp = new float32[uint32(width * height)]();
-    if (!sgm.Match(bytes_left, bytes_right, disparity, disparity_temp)) {
+    if (!sgm.Match(bytes_left, bytes_right, disparity, disparity_temp))
+    {
         std::cout << "SGM匹配失败！" << std::endl;
         return -2;
     }
-    
-    //Fetch census_left and census_right
-    //the pointer for census left and census right in order to get census data
+
+    // Fetch census_left and census_right
+    // the pointer for census left and census right in order to get census data
     auto census_left = new uint32[uint32(width * height)]();
     auto census_right = new uint32[uint32(width * height)]();
-    if (!sgm.GetCensus(census_left, census_right)) {
+    if (!sgm.GetCensus(census_left, census_right))
+    {
         std::cout << "GetCensus Failed!" << std::endl;
         return -2;
     }
-    
-    //Fetch cost_init
+
+    // Fetch cost_init
     sint32 disp_range = (sgm_option.max_disparity - sgm_option.min_disparity);
     auto cost_init = new uint8[uint32(width * height * disp_range)]();
-    if (!sgm.GetOriginCost(cost_init)) {
+    if (!sgm.GetOriginCost(cost_init))
+    {
         std::cout << "GetOriginCost Failed!" << std::endl;
         return -2;
     }
-    
-    //Fetch cost_aggr
+
+    // Fetch cost_aggr
     auto cost_aggr_lr = new uint8[uint32(width * height * disp_range)]();
     auto cost_aggr_rl = new uint8[uint32(width * height * disp_range)]();
-    if (!sgm.GetAggrCost(cost_aggr_lr, 1)) {
+    if (!sgm.GetAggrCost(cost_aggr_lr, 1))
+    {
         std::cout << "GetOriginCost Failed!" << std::endl;
         return -2;
     }
-    if (!sgm.GetAggrCost(cost_aggr_rl, 2)) {
+    if (!sgm.GetAggrCost(cost_aggr_rl, 2))
+    {
         std::cout << "GetOriginCost Failed!" << std::endl;
         return -2;
     }
-    
-    //Fetch my disparity
-    auto my_disparity = new uint32[uint32(width*height)]();
-    if (!sgm.GetDisparity(my_disparity)) {
+
+    // Fetch my disparity
+    auto my_disparity = new uint32[uint32(width * height)]();
+    if (!sgm.GetDisparity(my_disparity))
+    {
         std::cout << "GetMyDisparity Failed!" << std::endl;
     }
 
     // 显示视差图
     cv::Mat disp_mat = cv::Mat(height, width, CV_8UC1);
     float min_disp = width, max_disp = -width;
-    for (sint32 i = 0; i < height; i++) {
-        for (sint32 j = 0; j < width; j++) {
+    for (sint32 i = 0; i < height; i++)
+    {
+        for (sint32 j = 0; j < width; j++)
+        {
             const float32 disp = disparity[i * width + j];
-            if (disp != Invalid_Float) {
+            if (disp != Invalid_Float)
+            {
                 min_disp = std::min(min_disp, disp);
                 max_disp = std::max(max_disp, disp);
             }
         }
     }
-    for (sint32 i = 0; i < height; i++) {
-        for (sint32 j = 0; j < width; j++) {
+    for (sint32 i = 0; i < height; i++)
+    {
+        for (sint32 j = 0; j < width; j++)
+        {
             const float32 disp = disparity[i * width + j];
-            if (disp == Invalid_Float) {
+            if (disp == Invalid_Float)
+            {
                 disp_mat.data[i * width + j] = 0;
             }
-            else {
+            else
+            {
                 disp_mat.data[i * width + j] = static_cast<uchar>((disp - min_disp) / (max_disp - min_disp) * 255);
             }
         }
     }
-    
+
     cv::Mat disp_temp_mat = cv::Mat(height, width, CV_8UC1);
     float min_disp_temp = width, max_disp_temp = -width;
-    for (sint32 i = 0; i < height; i++) {
-        for (sint32 j = 0; j < width; j++) {
+    for (sint32 i = 0; i < height; i++)
+    {
+        for (sint32 j = 0; j < width; j++)
+        {
             const float32 disp_temp = disparity_temp[i * width + j];
-            if (disp_temp != Invalid_Float) {
+            if (disp_temp != Invalid_Float)
+            {
                 min_disp_temp = std::min(min_disp_temp, disp_temp);
                 max_disp_temp = std::max(max_disp_temp, disp_temp);
             }
         }
     }
-    for (sint32 i = 0; i < height; i++) {
-        for (sint32 j = 0; j < width; j++) {
+    for (sint32 i = 0; i < height; i++)
+    {
+        for (sint32 j = 0; j < width; j++)
+        {
             const float32 disp_temp = disparity_temp[i * width + j];
-            if (disp_temp == Invalid_Float) {
+            if (disp_temp == Invalid_Float)
+            {
                 disp_temp_mat.data[i * width + j] = 0;
             }
-            else {
+            else
+            {
                 disp_temp_mat.data[i * width + j] = static_cast<uchar>((disp_temp - min_disp_temp) / (max_disp_temp - min_disp_temp) * 255);
             }
         }
     }
-    
+
     // 保存结果txt
     std::FILE *FilePosition;
     std::FILE *FilePosition_temp;
     FilePosition = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/disparity.txt", "w");
     FilePosition_temp = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/disparity_temp.txt", "w");
-    std::cout<<"Start Writing Disparity..."<<std::endl;
-    for (sint32 i = 0 ; i < height ; i++)
+    std::cout << "Start Writing Disparity..." << std::endl;
+    for (sint32 i = 0; i < height; i++)
     {
-        for (sint32 j = 0 ; j < width ; j++)
+        for (sint32 j = 0; j < width; j++)
         {
             // std::cout<<"Hello!"<<std::endl;
-            if (j == width-1)
+            if (j == width - 1)
             {
                 std::fprintf(FilePosition, "%f\n", disparity[i * width + j]);
                 std::fprintf(FilePosition_temp, "%f\n", disparity_temp[i * width + j]);
@@ -215,9 +271,9 @@ int main(int argv, char** argc)
         }
     }
     std::fclose(FilePosition);
-    std::cout<<"Writing Disparity Finished!"<<std::endl;
-    
-    //Save Census left and Census right
+    std::cout << "Writing Disparity Finished!" << std::endl;
+
+    // Save Census left and Census right
     std::FILE *FilePosition_left_d;
     std::FILE *FilePosition_right_d;
     std::FILE *FilePosition_left_X;
@@ -227,7 +283,7 @@ int main(int argv, char** argc)
     FilePosition_left_X = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/census_left_X.txt", "w");
     FilePosition_right_X = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/census_right_X.txt", "w");
     std::cout << "Start Writing Census Left and Right..." << std::endl;
-    for (sint32 i = 0 ; i < height*width ; i++)
+    for (sint32 i = 0; i < height * width; i++)
     {
         std::fprintf(FilePosition_left_d, "%d\n", census_left[i]);
         std::fprintf(FilePosition_right_d, "%d\n", census_right[i]);
@@ -238,19 +294,20 @@ int main(int argv, char** argc)
     std::fclose(FilePosition_right_d);
     std::fclose(FilePosition_left_X);
     std::fclose(FilePosition_right_X);
-    std::cout<<"Writing Census Left and Right Finished!"<<std::endl;
-    
-    //Save origin cost
+    std::cout << "Writing Census Left and Right Finished!" << std::endl;
+
+    // Save origin cost
     std::FILE *FilePosition_cost_d;
     std::FILE *FilePosition_cost_X;
     FilePosition_cost_d = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/cost_init_d.txt", "w");
     FilePosition_cost_X = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/cost_init_X.txt", "w");
     std::cout << "Start Writing Census Left and Right..." << std::endl;
-    for (sint32 i = 0 ; i < height*width ; i++)
+    for (sint32 i = 0; i < height * width; i++)
     {
-        for (sint32 j = 0 ; j < disp_range ; j++)
+        for (sint32 j = 0; j < disp_range; j++)
         {
-            if (j == disp_range - 1) {
+            if (j == disp_range - 1)
+            {
                 std::fprintf(FilePosition_cost_d, "%d\n", cost_init[i * disp_range + j]);
                 std::fprintf(FilePosition_cost_X, "%02X\n", cost_init[i * disp_range + j]);
                 continue;
@@ -261,9 +318,9 @@ int main(int argv, char** argc)
     }
     std::fclose(FilePosition_cost_d);
     std::fclose(FilePosition_cost_X);
-    std::cout<<"Writing Origin Cost Finished!"<<std::endl;
-    
-    //Save aggregation cost
+    std::cout << "Writing Origin Cost Finished!" << std::endl;
+
+    // Save aggregation cost
     std::FILE *FilePosition_aggr_lr_d;
     std::FILE *FilePosition_aggr_rl_d;
     std::FILE *FilePosition_aggr_lr_X;
@@ -273,11 +330,12 @@ int main(int argv, char** argc)
     FilePosition_aggr_lr_X = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/aggr_lr_X.txt", "w");
     FilePosition_aggr_rl_X = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/aggr_rl_X.txt", "w");
     std::cout << "Start Writing Census Left and Right..." << std::endl;
-    for (sint32 i = 0 ; i < height*width ; i++)
+    for (sint32 i = 0; i < height * width; i++)
     {
-        for (sint32 j = 0 ; j < disp_range ; j++)
+        for (sint32 j = 0; j < disp_range; j++)
         {
-            if (j == disp_range - 1) {
+            if (j == disp_range - 1)
+            {
                 std::fprintf(FilePosition_aggr_lr_d, "%d\n", cost_aggr_lr[i * disp_range + j]);
                 std::fprintf(FilePosition_aggr_lr_X, "%08X\n", cost_aggr_lr[i * disp_range + j]);
                 std::fprintf(FilePosition_aggr_rl_d, "%d\n", cost_aggr_rl[i * disp_range + j]);
@@ -290,22 +348,22 @@ int main(int argv, char** argc)
             std::fprintf(FilePosition_aggr_rl_X, "%08X\t", cost_aggr_rl[i * disp_range + j]);
         }
     }
-    
-    //Save my disparity
+
+    // Save my disparity
     std::FILE *FilePosition_my_disp;
     FilePosition_my_disp = std::fopen("/home/zty/python_program/awesome-sgbm/Data/FPGA_proc/my_disparity.txt", "w");
     std::cout << "Start Writing My Disparity..." << std::endl;
-    for (sint32 i = 0; i < height*width; i++)
+    for (sint32 i = 0; i < height * width; i++)
     {
         std::fprintf(FilePosition_my_disp, "%08X\n", my_disparity[i]);
     }
-    
-    cv::namedWindow("视差图",1);
-    cv::namedWindow("视差图TEMP!",1);
-    cv::namedWindow("视差图-伪彩",1);
-    cv::namedWindow("视差图-伪彩TEMP!",1);
-    cv::imshow("视差图", disp_mat);    
-    cv::imshow("视差图TEMP!", disp_temp_mat);   
+
+    cv::namedWindow("视差图", 1);
+    cv::namedWindow("视差图TEMP!", 1);
+    cv::namedWindow("视差图-伪彩", 1);
+    cv::namedWindow("视差图-伪彩TEMP!", 1);
+    cv::imshow("视差图", disp_mat);
+    cv::imshow("视差图TEMP!", disp_temp_mat);
 
     cv::Mat disp_color;
     applyColorMap(disp_mat, disp_color, cv::COLORMAP_JET);
@@ -313,25 +371,25 @@ int main(int argv, char** argc)
     cv::Mat disp_temp_color;
     applyColorMap(disp_temp_mat, disp_temp_color, cv::COLORMAP_JET);
     cv::imshow("视差图-伪彩TEMP!", disp_temp_color);
-   
+
     // 保存结果bmp
-    std::string disp_map_path = argc[2]; disp_map_path += ".d.bmp";
-    std::string disp_color_map_path = argc[2]; disp_color_map_path += ".c.bmp";
+    std::string disp_map_path = argc[2];
+    disp_map_path += ".d.bmp";
+    std::string disp_color_map_path = argc[2];
+    disp_color_map_path += ".c.bmp";
     cv::imwrite(disp_map_path, disp_mat);
     cv::imwrite(disp_color_map_path, disp_color);
 
     cv::waitKey(0);
 
-   //···············································································//
-   // 释放内存
-   delete[] disparity;
-   disparity = nullptr;
-   delete[] bytes_left;
-   bytes_left = nullptr;
-   delete[] bytes_right;
-   bytes_right = nullptr;
+    // ···············································································//
+    //  释放内存
+    delete[] disparity;
+    disparity = nullptr;
+    delete[] bytes_left;
+    bytes_left = nullptr;
+    delete[] bytes_right;
+    bytes_right = nullptr;
 
-
-   
-   return 1;
+    return 1;
 }
